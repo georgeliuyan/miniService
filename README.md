ff707804471d9e656e050f16f5a6ba688e939d2c
liuyan <liuyan@geovis.com.cn>
2018-10-09 10:08:42 +0800

    springboot中配置swagger2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # miniService
 serviceTest
+
+#swagger
+swagger2 : http://127.0.0.1:18080/pro/swagger-ui.html
--- src/main/java/com/geovis/pro/rest/okHttpRest.java
+++ src/main/java/com/geovis/pro/rest/okHttpRest.java
@@ -7,22 +7,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.geovis.pro.resultEntity.ResultEntity;
 import com.geovis.pro.util.OkHttpUtil;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import okhttp3.HttpUrl;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 
 @Controller
 @RequestMapping("/rest")
+@Api("swaggerDemoController相关的api")
 public class okHttpRest {
 	public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 	OkHttpClient client = new OkHttpClient();
 	
-	
+	@ApiOperation(value = "根据id查询学生信息", notes = "查询数据库中某个的学生信息")
+//    @ApiImplicitParam(name = "id", value = "学生ID", paramType = "path", required = true, dataType = "Integer")
+//    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	@GetMapping("/okhttppost")
 	@ResponseBody
     public ResultEntity okhttppost(){
