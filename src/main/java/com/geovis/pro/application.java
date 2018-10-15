7f20d59c1c712ee8356a7c191525ce2248c5bd41
liuyan <liuyan@geovis.com.cn>
2018-10-15 15:58:12 +0800

    整理代码规范

--- src/main/java/com/geovis/pro/application.java
+++ src/main/java/com/geovis/pro/application.java
@@ -5,9 +5,19 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-@EnableAutoConfiguration
-@SpringBootApplication
-@EnableCaching
+@SpringBootApplication  
+/*
+ * Spring Boot 应用标注某个类上说明这个类是SpringBoot主配置类,注解包括如下：
+ * 
+ * @SpringBootConfiguration ：Spring Boot配置类，标注上某个类上，表示这个一个SpringBoot 配置类
+ * @EnableAutoConfiguration ：开启自动配置功能，以前需要配置的东西，SpringBoot帮我们自动配置；
+ * @ComponentScan(excludeFilters={@Filter(type=CUSTOM, classes={TypeExcludeFilter.class})})
+ * @Target(value={TYPE})
+ * @Retention(value=RUNTIME)
+ * @Documented
+ * @Inherited
+ */
+//@EnableCaching 缓存机制
 public class application {
 	
  	public static void main(String[] args) {
--- src/main/java/com/geovis/pro/rest/redisRest.java
+++ src/main/java/com/geovis/pro/rest/redisRest.java
@@ -1,7 +1,5 @@
 package com.geovis.pro.rest;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +9,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.geovis.pro.common.ResultEntity;
 import com.geovis.pro.model.Girl;
 import com.geovis.pro.service.RedisService;
-import com.geovis.pro.util.OkHttpUtil;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import okhttp3.HttpUrl;
 
 @Controller
 @RequestMapping("/rest")
