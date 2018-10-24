6ab6308b2df1e898c4aeb712cb62ac37733d1e0c
liuyan <liuyan@geovis.com.cn>
2018-10-24 13:52:12 +0800

    @Mapper注解的版本描述

--- src/main/java/com/geovis/pro/dao/SysUserDao.java
+++ src/main/java/com/geovis/pro/dao/SysUserDao.java
@@ -2,10 +2,12 @@ package com.geovis.pro.dao;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.cache.annotation.Cacheable;
+import org.mybatis.spring.annotation.MapperScan;
 
 import com.geovis.pro.model.SysUserEntity;
 
+//从mybatis3.4.0开始加入了@Mapper注解，目的就是为了不再写mapper映射文件
+//mybatis-spring-boot-starter的1.1.1版本以后新增的，之前为@MapperScan
 @Mapper
 public interface SysUserDao {
     void save(SysUserEntity user);
